09b0d113276ee688b1003f0ab6483b44f1d238c4
jachs <799516423@qq.com>
2019-10-18 17:42:23 +0800

    键盘点击

---
+++ src/main/java/com/jachs/desktop/client/ClickKey.java
@@ -0,0 +1,13 @@
+package com.jachs.desktop.client;
+/****
+ * 键盘点击
+ * @author zhanchaohan
+ *
+ */
+public class ClickKey implements Runnable{
+
+	public void run() {
+		
+	}
+
+}
---
+++ src/main/java/com/jachs/desktop/server/ClickKeyEvent.java
@@ -0,0 +1,13 @@
+package com.jachs.desktop.server;
+/*****
+ * 键盘点击
+ * @author zhanchaohan
+ *
+ */
+public class ClickKeyEvent implements Runnable{
+
+	public void run() {
+		
+	}
+
+}

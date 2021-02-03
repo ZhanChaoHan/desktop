0e3723605b1d0900b940b4bf6c745c17731b0dfa
799516423@qq.com <github_jachs22>
2021-02-03 21:40:08 +0800

    bfbhqe

--- src/main/java/com/jachs/desktop/configer/StaticConfigure.java
+++ src/main/java/com/jachs/desktop/configer/StaticConfigure.java
@@ -14,6 +14,6 @@ public class StaticConfigure {
 	//客戶端窗口圖片素材地址
 	 public static final String CLIENTIMAGEPATH = new File("").getAbsolutePath() + File.separator + "image"+ File.separator;
 	 
-	 public static ManEntity MANENTITY;
-	 public static LinkedList<Integer>PORTLIST=new LinkedList<Integer>();
+	 public static ManEntity MANENTITY;//初始化对象
+	 public static LinkedList<Integer>PORTLIST=new LinkedList<Integer>();//初始化端口集合
 }

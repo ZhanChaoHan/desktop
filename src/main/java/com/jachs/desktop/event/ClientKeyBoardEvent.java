37dfe851cc23b58e4fb434aa6f44f76cede9c9dd
jachs <799516423@qq.com>
2021-02-10 16:46:52 +0800

    控制台输出

--- src/main/java/com/jachs/desktop/event/ClientKeyBoardEvent.java
+++ src/main/java/com/jachs/desktop/event/ClientKeyBoardEvent.java
@@ -17,12 +17,15 @@ import com.jachs.desktop.entity.SendInfoType;
 public class ClientKeyBoardEvent implements KeyListener{
 	//第二执行
 	public void keyTyped(KeyEvent e) {
+	    System.out.println ("keyTyped:"+ e.getID ()+"\t"+e.getKeyCode ()+"\t"+e.getWhen () );
 	}
 	//按下一个键盘事件,最先触发
 	public void keyPressed(KeyEvent e) {
+	    System.out.println ("keyPressed:"+ e.getID ()+"\t"+e.getKeyCode ()+"\t"+e.getWhen () );
 	}
 	//最后释放键盘事件，最后触发
 	public void keyReleased(KeyEvent e) {
+	    System.out.println ("keyReleased:"+ e.getID ()+"\t"+e.getKeyCode ()+"\t"+e.getWhen () );
 		System.out.println(e.getKeyChar()+"keyReleased");
 		Event keyBoardEvent=new ClickEventEn(SendInfoType.KeyBoard,e.getKeyChar());
 		try {
